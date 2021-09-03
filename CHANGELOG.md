fda4f9a3a9d375ca35da91215735de5b846b254a
zmtzawqlp <zmtzawqlp@live.com>
2021-09-03 22:15:48 +0800

    ## 0.2.0-non-null-safety * Public classes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.2.0-non-null-safety
+
+* Public classes
 ## 0.1.0-non-null-safety
 
 * First Release.
--- lib/flex_grid.dart
+++ lib/flex_grid.dart
@@ -7,4 +7,6 @@ export 'src/controller/drag_hold_controller.dart';
 export 'src/controller/scroll_controller.dart';
 export 'src/controller/scroll_physics.dart';
 export 'src/flex_grid.dart';
+export 'src/horizontal_sync_scroll_minxin.dart';
 export 'src/style/style.dart';
+export 'src/typedef.dart';
